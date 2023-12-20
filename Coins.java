311734a5e648f0ff2e981b23adfc3259d265d019
Omer Katz <omer.katz@post.runi.ac.il>
2023-12-20 11:08:34 +0200

    Omer's Homework 1

--- Coins.java
+++ Coins.java
@@ -5,5 +5,7 @@
 public class Coins {
 	public static void main(String[] args) {
 		// Put your code here
+		int x = Integer.parseInt(args[0]);
+		System.out.println("Use" + " " + (x/25) + " " + "quarters and" + " " + (x%25) + " " + "cents");
 	}
 }
--- GenThree.java
+++ GenThree.java
@@ -5,5 +5,18 @@
 public class GenThree {
 	public static void main(String[] args) {
 		// Put your code here	
-	}
+	    int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		double ran1, ran2, ran3;
+		ran1 = Math.random();
+		ran2 = Math.random();
+		ran3 = Math.random();
+		int n1 = (int) (((b - a) * ran1) + a);
+		int n2 = (int) (((b - a) * ran2) + a);
+		int n3 = (int) (((b - a) * ran3) + a);
+		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println(n3);
+		System.out.println("The minimal generated number was " + Math.min(n1,Math.min(n2,n3)));
+	}   
 }
