cb490b640adc7e8bb06fb58bb8ffb98b7c1f0355
Omer Katz <omer.katz@post.runi.ac.il>
2023-12-20 14:52:17 +0200

    Omer's Homework 1

--- GenThree.java
+++ GenThree.java
@@ -15,24 +15,23 @@ public class GenThree {
 		// its not clear if the first has to be smaller, so in that case we will make sure which one is the bigger
 		if (b > a){
 			r = b - a;
-		int n1 = (int) ((r * ran1) + a);
-		int n2 = (int) ((r * ran2) + a);
-		int n3 = (int) ((r * ran3) + a);
-		System.out.println(n1);
-		System.out.println(n2);
-		System.out.println(n3);
-		System.out.println("The minimal generated number was " + Math.min(n1,Math.min(n2,n3)));
+			int n1 = (int) ((r * ran1) + a);
+			int n2 = (int) ((r * ran2) + a);
+			int n3 = (int) ((r * ran3) + a);
+			System.out.println(n1);
+			System.out.println(n2);
+			System.out.println(n3);
+			System.out.println("The minimal generated number was " + Math.min(n1,Math.min(n2,n3)));
 		}
 		else {
 			r = a - b;
 			int n1 = (int) ((r * ran1) + b);
-		int n2 = (int) ((r * ran2) + b);
-		int n3 = (int) ((r * ran3) + b);
-		System.out.println(n1);
-		System.out.println(n2);
-		System.out.println(n3);
-		System.out.println("The minimal generated number was " + Math.min(n1,Math.min(n2,n3)));
-			
+			int n2 = (int) ((r * ran2) + b);
+			int n3 = (int) ((r * ran3) + b);
+			System.out.println(n1);
+			System.out.println(n2);
+			System.out.println(n3);
+			System.out.println("The minimal generated number was " + Math.min(n1,Math.min(n2,n3)));	
 	}   
 }
 }
